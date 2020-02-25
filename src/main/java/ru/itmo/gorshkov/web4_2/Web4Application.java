596b2525d5ff1e39e493181cb97529796308ca4a
artem <tyema25@mail.ru>
2020-02-25 13:05:31 +0300

    Spring work. Made a web archive React don't work

--- src/main/java/ru/itmo/gorshkov/web4_2/Web4Application.java
+++ src/main/java/ru/itmo/gorshkov/web4_2/Web4Application.java
@@ -2,9 +2,17 @@ package ru.itmo.gorshkov.web4_2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Web4Application {
+public class Web4Application  extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Web4Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Web4Application.class, args);
--- src/main/java/ru/itmo/gorshkov/web4_2/WebConfig.java
+++ src/main/java/ru/itmo/gorshkov/web4_2/WebConfig.java
@@ -1,19 +1,21 @@
 package ru.itmo.gorshkov.web4_2;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/static/");
+    }
+
+    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index");
         registry.addViewController("/main").setViewName("main");
     }
 }
